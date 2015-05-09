9a78125a8880cd30b7909740f735d6a2ed511a3b
012345678 <ynl431@gmail.com>
2015-05-09 13:34:37 -0400

    suggests a new format of authKeys.py

---
+++ authKeys.py
@@ -0,0 +1,34 @@
+#skeleton
+
+
+def getA(i):  # consumer_key
+    if (i == 431):
+        return ''
+    elif (i == 853):
+        return ''
+    else:
+        return ''
+
+def getB(i):  # consumer_secret
+    if (i == 431):
+        return ''
+    elif (i == 853):
+        return ''
+    else:
+        return ''
+
+def getC(i):  # access_token_key
+    if (i == 431):
+        return ''
+    elif (i == 853):
+        return ''
+    else:
+        return ''
+
+def getD(i):  # access_token_secret
+    if (i == 431):
+        return ''
+    elif (i == 853):
+        return ''
+    else:
+        return ''
