a45ff5a21890f422a9e57d2abfb3a913e837573c
012345678 <ynl431@gmail.com>
2015-05-09 13:37:33 -0400

    updated to match new authKey format

--- basicInfo.py
+++ basicInfo.py
@@ -2,20 +2,21 @@ from twython import Twython	# Required to retrieve Users/show.json
 from datetime import datetime
 import authKeys
 
-def showBasicInfo(userName):
+def showBasicInfo(userName, keySet):
 
     #Creating a new Twython object
-    show_user_twython = Twython(app_key = authKeys.getA(),
-                        app_secret = authKeys.getB(),
-                        oauth_token = authKeys.getC(),
-                        oauth_token_secret = authKeys.getD())
+    show_user_twython = Twython(
+        app_key = authKeys.getA(keySet),
+        app_secret = authKeys.getB(keySet),
+        oauth_token = authKeys.getC(keySet),
+        oauth_token_secret = authKeys.getD(keySet))
 
     #Creating the show_user json dictionary
     show_user = show_user_twython.show_user(screen_name=userName)
 
-    print 'Name: ' + show_user.get('name')				# Name of the account owner
-    print 'Screen Name: ' + show_user.get('screen_name')		# Screen name # This is a bit redundant
-    print 'User ID: ' + show_user.get('id_str')			# Numeric id of the account
+    print 'Name: ' + show_user.get('name')				    # Name of the account owner
+    print 'Screen Name: ' + show_user.get('screen_name')	# Screen name # This is a bit redundant
+    print 'User ID: ' + show_user.get('id_str')			    # Numeric id of the account
 
     #print 'Account Creation: ' + show_user.get('created_at')	# Date the account was created in twitter datetime format
     # Converting from twitter datetime format to python datetime format
@@ -29,4 +30,4 @@ def showBasicInfo(userName):
     # Total number of tweets
     print 'Total Tweets: ' + str(show_user.get('statuses_count'))
     # Average number of tweets each day
-    print 'Average Number of Tweets Daily: ' + str('%.2f'%(float(show_user.get('statuses_count')) / float(age)))
+    print 'Average Number of Tweets Per Day: ' + str('%.2f'%(float(show_user.get('statuses_count')) / float(age)))
